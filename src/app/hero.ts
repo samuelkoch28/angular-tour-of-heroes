fdfd39ad4f3abfb99c037201a2b40f36e5bfe938
Samuel Koch <samuel_koch@sluz.ch>
2024-01-08 14:02:25 +0100

    Made property superkraft optional

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,5 +1,5 @@
 export interface Hero {
   id: number;
   name: string;
-  superkraft: string;
+  superkraft?: string;
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice', superkraft: "Fliegen" },
+  { id: 12, name: 'Dr. Nice' },
   { id: 13, name: 'Bombasto', superkraft: "Fliegen" },
-  { id: 14, name: 'Celeritas', superkraft: "Fliegen" },
+  { id: 14, name: 'Celeritas',},
   { id: 15, name: 'Magneta', superkraft: "Fliegen" },
-  { id: 16, name: 'RubberMan', superkraft: "Fliegen" },
-  { id: 17, name: 'Dynama', superkraft: "Fliegen" },
-  { id: 18, name: 'Dr. IQ', superkraft: "Fliegen" },
-  { id: 19, name: 'Magma', superkraft: "Fliegen" },
+  { id: 16, name: 'RubberMan', },
+  { id: 17, name: 'Dynama', },
+  { id: 18, name: 'Dr. IQ', },
+  { id: 19, name: 'Magma', },
   { id: 20, name: 'Tornado', superkraft: "Fliegen" }
 ];
